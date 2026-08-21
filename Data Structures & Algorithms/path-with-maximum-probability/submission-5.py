aa3e5890f46b4a3fb9b71486a2232d3c5b0a458d
Debayan Mitra <52120078+debayanmitra1993-data@users.noreply.github.com>
2026-08-21 21:08:51 +0530

    Add: path-with-maximum-probability - submission-5

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-5.py
@@ -0,0 +1,32 @@
+import heapq
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        graph = {}
+        maxpaths = {}
+        for node in range(n):
+            graph[node] = []
+            maxpaths[node] = float("-inf")
+
+        for idx in range(len(edges)):
+            srcnode, destnode = edges[idx][0], edges[idx][1]
+            prob_edge = succProb[idx]
+            graph[srcnode].append((destnode, prob_edge))
+            graph[destnode].append((srcnode, prob_edge))
+        
+        pq = []
+        pq.append((-1.0, start_node))
+        visited = set()
+        while len(pq) > 0:
+            probnode, node = heapq.heappop(pq)
+            if -probnode > maxpaths[node]:
+                maxpaths[node] = -probnode
+                for child in graph[node]:
+                    childnode, childprobedge = child[0], child[1]
+                    if childnode not in visited:
+                        heapq.heappush(pq, (probnode * childprobedge, childnode))
+            visited.add(node)
+        # print("maxpaths = ", maxpaths)
+        if maxpaths[end_node] == float("-inf"):
+            return 0.0
+        return maxpaths[end_node]
